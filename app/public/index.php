92f55cf573e8004c2a18db515378c3e62ad90976
adif_kirito <97230173+adif-kirito@users.noreply.github.com>
2024-02-02 00:49:22 +0800

    update

--- app/public/index.php
+++ app/public/index.php
@@ -1,2 +1,9 @@
 <?php
-phpinfo();
+
+try {
+    $dbh = new PDO('mysql:host=e_db;dbname=my_database', 'root', 'root');
+    echo '<h4 style="color: green">Connected to database sucessfully.</h4>';
+}
+catch (PDOException $e) {
+    echo '<h4 style="color: red">Fail to connect to database.</h4>';
+}
